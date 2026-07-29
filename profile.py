a9fc08bacb55e76bcbfba8c4415d0078f94bfe03
Uwa-Fame <ebuwafamous@gmail.com>
2026-07-29 20:34:16 +0100

    Complete Exercise 1: Personal Info Script

---
+++ profile.py
@@ -0,0 +1,19 @@
+
+# This programm stores the datas of a user (NAME AGE & Favourite number) 
+# I have also used a "print TYPE function to declare the type of datas stored in each of the variables.
+# The program also calculat the number of ones' age in x years time.
+Name = "Famous Ebuwa"
+Age = 38
+Favorite_number = 14
+
+print(f" Myname is {Name}. I am {Age} years old and my favourite number is {Favorite_number}")
+
+print (type(Name))
+print (type(Age))
+print (type(Favorite_number))
+
+Age_in_10_years = Age * 10
+print (f"10 years from now, I will be {Age_in_10_years} years old.")
+
+Age_in_5_years = Age * 5
+print(f"5 years from now, I will be {Age_in_5_years} years old.")
