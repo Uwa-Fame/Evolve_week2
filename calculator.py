773ddbbe8899e727aba233db2ed9c7f3dd9aee62
Uwa-Fame <ebuwafamous@gmail.com>
2026-08-02 05:00:25 +0100

    Addaproper docstring to every function

--- calculator.py
+++ calculator.py
@@ -1,15 +1,58 @@
 
 def add (a, b):
+    """ A function that adds two numbers together
+
+    Args:
+    index(a) (int): A single lower case that represent the first number
+    index(b) (int): A single lower case that represent the second number
+   
+    Returns:
+    the addition of two numbers
+
+    """
     return a + b
 
+
 def subtract(a, b):
+    
+    """ A function that adds two numbers together
+
+    Args:
+    index(a) (int): A single lower case that represent the first number
+    index(b) (int): A single lower case that represent the second number
+   
+    Returns:
+    the difference of two number
+
+    """
     return a - b
+    
 
     
 def multiply (a, b):
+    """
+    A function that returns the product of two numbers.
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The product of the two numbers.
+    """
     return a * b
+    
 # divide only if the number is greater than zero
 def divide(a, b):
+    """
+    A function that divides a number if the number is greater than zero.
+    Args:
+        a (int): the first number
+        b (int): the second number
+   
+    Returns:
+    the difference of two numbers 
+"""
+    
     if b == 0:
         return "cannot devide by zero"
     return a / b
